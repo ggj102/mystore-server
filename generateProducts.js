34dced3365b5b02976c2b77035fc7276c036cd8b
gwang-yeom.kim <gwang-yeom.kim@navercorp.com>
2024-05-01 18:00:54 +0900

    refactor: 크롤링 데이터 DB에 생성

--- generateProducts.js
+++ generateProducts.js
@@ -1,34 +1,67 @@
 const { PrismaClient } = require("@prisma/client");
-
+const productCrawlData = require("./productCrawlData");
 const prisma = new PrismaClient();
 
-async function main() {
-  // 상품 데이터 생성
-  const products = [
-    { name: "Product 1", price: 10.99, category: "Electronics" },
-    { name: "Product 2", price: 24.99, category: "Clothing" },
-    { name: "Product 3", price: 14.99, category: "Books" },
-    { name: "Product 4", price: 49.99, category: "Home & Kitchen" },
-    { name: "Product 5", price: 7.99, category: "Toys" },
-    { name: "Product 6", price: 39.99, category: "Electronics" },
-    { name: "Product 7", price: 19.99, category: "Clothing" },
-    { name: "Product 8", price: 29.99, category: "Books" },
-    { name: "Product 9", price: 54.99, category: "Home & Kitchen" },
-    { name: "Product 10", price: 12.99, category: "Toys" },
-  ];
+async function deleteProductAndDetailById(id) {
+  const transaction = await prisma.$transaction([
+    prisma.Product_Detail.delete({
+      where: {
+        id,
+      },
+    }),
+    prisma.Product.delete({
+      where: {
+        id,
+      },
+    }),
+  ]);
 
-  // 상품 데이터를 데이터베이스에 생성
-  for (let productData of products) {
-    await prisma.product.create({ data: productData });
-  }
-
-  console.log("10 products created successfully!");
+  console.log(`id가 ${id}인 제품과 id가 ${id}인 상세정보가 삭제되었습니다.`);
+  return transaction;
 }
 
-main()
-  .catch((err) => {
-    console.error("Error creating products:", err);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
+// deleteProductAndDetailById(2)
+//   .then(() => {
+//     console.log("삭제가 완료되었습니다.");
+//   })
+//   .catch((error) => {
+//     console.error("삭제 중 오류가 발생했습니다:", error);
+//   });
+
+async function createProducts() {
+  try {
+    // 제품 데이터 일괄 삽입
+    const productsData = productCrawlData;
+
+    for (let i = 0; i < productsData.length; i++) {
+      const productData = productsData[i];
+
+      const time_sale = productData.time_sale
+        ? new Date(productData.time_sale)
+        : null;
+
+      await prisma.product.create({
+        data: {
+          ...productData,
+          time_sale,
+          release_date: new Date(productData.release_date),
+          product_detail: {
+            create: { ...productData.product_detail },
+          },
+        },
+        include: { product_detail: true }, // product_detail을 반환하도록 include 지정
+      });
+    }
+
+    console.log("Products가 성공적으로 저장되었습니다.");
+  } catch (error) {
+    console.error("Products 저장 중 오류가 발생했습니다:", error);
+    throw error;
+  }
+}
+
+createProducts()
+  .then()
+  .catch((error) => {
+    console.error("Products 저장 중 오류가 발생했습니다:", error);
   });
