121354b996e79d2c05dce04e1fcdaa12d6307876
kjkim <kjkim@xsinc.co.kr>
2024-05-31 11:22:39 +0900

    feat: user/order route 추가 - 유저 주문 데이터 get - 유저 주문 데이터 삭제 delete

--- routes/user.js
+++ routes/user.js
@@ -31,4 +31,62 @@ router.get("/", authenticateToken, async (req, res) => {
   }
 });
 
+router.get("/order", authenticateToken, async (req, res) => {
+  try {
+    const user_id = getAccessTokenUserId(req);
+    const orders = await prisma.Order.findMany({
+      where: {
+        user_id,
+      },
+    });
+
+    const orderData = [];
+
+    for (const order of orders) {
+      const orderItems = await prisma.Order_Item.findMany({
+        where: {
+          order_id: order.id,
+        },
+      });
+
+      const remain = orderItems.length - 1;
+      const item = orderItems[0];
+
+      const data = {
+        ...order,
+        order_name: item.item_name,
+        image_path: item.image_path,
+        remain,
+      };
+
+      orderData.push(data);
+    }
+
+    res.json(orderData);
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    res.status(500).json({ error: "An error occurred while fetching data" });
+  }
+});
+
+router.delete("/order", authenticateToken, async (req, res) => {
+  const order_id = parseInt(req.body.order_id);
+
+  try {
+    const user_id = getAccessTokenUserId(req);
+
+    await prisma.Order.delete({
+      where: {
+        user_id: user_id,
+        id: order_id,
+      },
+    });
+
+    res.json({ message: "success" });
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    res.status(500).json({ error: "An error occurred while fetching data" });
+  }
+});
+
 module.exports = router;
