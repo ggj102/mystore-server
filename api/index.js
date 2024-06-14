e5c0ed2cd0133804648cf7d7e0e747f56a364aa3
kjkim <kjkim@xsinc.co.kr>
2024-06-14 22:14:58 +0900

    코드 수정

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cookieParser());
 app.use(bodyParser.json());
