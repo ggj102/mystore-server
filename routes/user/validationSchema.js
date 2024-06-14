1e8588338074304d0946e41fbaddcc1df0e6bfc0
kjkim <kjkim@xsinc.co.kr>
2024-06-14 18:25:38 +0900

    userDeliveryAddressSchema 수정

--- routes/user/validationSchema.js
+++ routes/user/validationSchema.js
@@ -1,6 +1,7 @@
 const yup = require("yup");
 
 const userDeliveryAddressSchema = yup.object().shape({
+  name: yup.string(),
   recipient: yup.string().required(),
   address: yup.string().required(),
   zone_code: yup.string().matches(/^\d+$/).required(),
@@ -8,7 +9,6 @@ const userDeliveryAddressSchema = yup.object().shape({
   phone_prefix: yup.string().matches(/^\d+$/).min(2),
   phone_start: yup.string().matches(/^\d+$/).required().min(3),
   phone_end: yup.string().matches(/^\d+$/).required().min(4),
-  message_index: yup.string().matches(/^\d+$/).max(1),
   direct_message: yup.string(),
   is_default: yup.boolean(),
 });
