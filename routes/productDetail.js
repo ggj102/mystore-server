d07df41d1e2eafb21b364762f15a0c167b7906f9
kjkim <kjkim@xsinc.co.kr>
2024-06-18 00:46:07 +0900

    refactor: [ProductDetail] get data select 로직 개선

--- routes/productDetail.js
+++ routes/productDetail.js
@@ -8,31 +8,16 @@ router.get("/:id", async (req, res) => {
   try {
     const id = parseInt(req.params.id);
 
-    const transaction = await prisma.$transaction([
-      prisma.Product.findUnique({
-        where: {
-          id,
-        },
-      }),
-      prisma.Product_Detail.findUnique({
-        where: {
-          id,
-        },
-      }),
-      prisma.Product_Option.findMany({
-        where: {
-          item_id: id,
-        },
-      }),
-    ]);
+    const detailData = await prisma.Product.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        product_detail: true,
+        product_option: true,
+      },
+    });
 
-    const detailData = {
-      ...transaction[0],
-      product_detail: { ...transaction[1] },
-      product_option: [...transaction[2]],
-    };
-
-    // 데이터를 JSON 형식으로 응답합니다.
     res.json(detailData);
   } catch (error) {
     console.error("Error fetching products:", error);
